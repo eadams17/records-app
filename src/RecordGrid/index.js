b1869a499fea9d02d7a34d69111b7bb3b7bec1f3
eadams17 <erictayloradams@gmail.com>
2019-06-18 20:36:08 -0400

    add edit functionality

--- src/RecordGrid/index.js
+++ src/RecordGrid/index.js
@@ -1,22 +1,24 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Record from "../Record";
 import styles from "./style.module.css";
 
-class RecordGrid extends PureComponent {
-  render() {
-    const { allRecords, filteredRecords } = this.props;
-    const records = filteredRecords ? filteredRecords : allRecords;
-
-    return (
-      <div className={styles.container}>
-        {records.map((record, i) => {
-          return (
-            <Record key={i} record={record} isLast={i === records.length - 1} />
-          );
-        })}
-      </div>
-    );
-  }
+function RecordGrid({ allRecords, filteredRecords, updateRecords }) {
+  const records = filteredRecords ? filteredRecords : allRecords;
+  return (
+    <div className={styles.container}>
+      {records.map((record, i) => {
+        return (
+          <Record
+            key={i}
+            record={record}
+            allRecords={allRecords}
+            updateRecords={updateRecords}
+            isLast={i === records.length - 1}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default RecordGrid;
--- src/UpdateRecord/index.js
+++ src/UpdateRecord/index.js
@@ -3,30 +3,111 @@ import styles from "./style.module.css";
 import {
   Button,
   Modal,
-  ModalHeader,
   ModalBody,
   ModalFooter,
-  InputGroup,
+  FormGroup,
   Input
 } from "reactstrap";
 
 class EditModal extends PureComponent {
+  state = {
+    album: this.props.record.album_title,
+    artist: this.props.record.artist.name,
+    year: this.props.record.year,
+    condition: this.props.record.condition
+  };
+
+  updateField = (e, field) => {
+    this.setState({ [field]: e.target.value });
+  };
+
+  updateSingleRecord() {
+    const { allRecords, record, updateRecords, toggle } = this.props;
+    const { album, artist, year, condition } = this.state;
+    const recordIndex = allRecords.findIndex(
+      recordItem => recordItem.album_title === record.album_title
+    );
+    let updatedRecords = allRecords;
+    updatedRecords[recordIndex] = {
+      album_title: album,
+      artist: { name: artist, id: allRecords[recordIndex].artist.id },
+      condition: condition,
+      year: year
+    };
+    console.log("record", updatedRecords[recordIndex]);
+    updateRecords(updatedRecords);
+    toggle();
+  }
+  updateMultipleRecords() {
+    const { allRecords, record, updateRecords, toggle } = this.props;
+    const { artist } = this.state;
+    const indices = allRecords.reduce((arr, recordItem, index) => {
+      recordItem.artist.id === record.artist.id && arr.push(index);
+      return arr;
+    }, []);
+    let updatedRecords = allRecords;
+    for (let i = 0; i < indices.length; i++) {
+      const index = indices[i];
+      const album = allRecords[index].album_title;
+      const condition = allRecords[index].condition;
+      const year = allRecords[index].year;
+      updatedRecords[index] = {
+        album_title: album,
+        artist: {
+          name: artist,
+          id: record.artist.id
+        },
+        condition: condition,
+        year: year
+      };
+    }
+    updateRecords(updatedRecords);
+    toggle();
+  }
+
+  checkForMultipleArtistEntries() {
+    const { allRecords, record } = this.props;
+    return (
+      allRecords.filter(
+        recordEntry => recordEntry.artist.id === record.artist.id
+      ).length > 1
+    );
+  }
+
+  handleSubmit = () => {
+    const multipleEntriesExist = this.checkForMultipleArtistEntries();
+    const artistUpdated = this.state.artist === this.props.record.artist.name;
+    console.log("1", multipleEntriesExist);
+    console.log("2", artistUpdated);
+    if (!multipleEntriesExist && artistUpdated) {
+      this.updateSingleRecord();
+    } else {
+      this.updateMultipleRecords();
+    }
+  };
   render() {
+    console.log("state", this.state);
     const { record, visible, toggle } = this.props;
 
     return (
       <div>
         <Modal isOpen={visible} toggle={toggle} className={styles.modal}>
-          <ModalHeader toggle={toggle} charCode="🎵">
-            Edit Album Information
-          </ModalHeader>
+          <div className={styles.modalHeaderContainer}>
+            <p className={styles.modalHeader}>Edit Album Information</p>
+            <i
+              id={styles.closeIcon}
+              className="fas fa-compact-disc"
+              onClick={toggle}
+            />
+          </div>
           <ModalBody>
-            <InputGroup className={styles.detailsContainer}>
+            <FormGroup className={styles.detailsContainer}>
               <div className={styles.row}>
                 <li className={styles.label}>Album</li>
                 <Input
                   defaultValue={record.album_title}
                   className={styles.info}
+                  onChange={value => this.updateField(value, "album")}
                 />
               </div>
               <div className={styles.row}>
@@ -34,6 +115,7 @@ class EditModal extends PureComponent {
                 <Input
                   defaultValue={record.artist.name}
                   className={styles.info}
+                  onChange={value => this.updateField(value, "artist")}
                 />
               </div>
               <div className={styles.row}>
@@ -41,18 +123,23 @@ class EditModal extends PureComponent {
                 <Input
                   defaultValue={record.condition}
                   className={styles.info}
+                  onChange={value => this.updateField(value, "condition")}
                 />
               </div>
               <div className={styles.row}>
                 <li className={styles.label}>Year</li>
-                <Input defaultValue={record.year} className={styles.info} />
+                <Input
+                  defaultValue={record.year}
+                  className={styles.info}
+                  onChange={value => this.updateField(value, "year")}
+                />
               </div>
-            </InputGroup>
+            </FormGroup>
           </ModalBody>
           <ModalFooter>
-            <Button className={styles.button} onClick={toggle}>
+            <Button className={styles.button} onClick={this.handleSubmit}>
               Save
-            </Button>{" "}
+            </Button>
             <Button className={styles.button} onClick={toggle}>
               Cancel
             </Button>
@@ -62,28 +149,5 @@ class EditModal extends PureComponent {
     );
   }
 }
-
-// <div className={styles.detailsContainer}>
-//   <div className={styles.row}>
-//     <li className={styles.label}>Album</li>
-//     <li className={styles.info}>
-//       {record.album_title}
-//     </li>
-//   </div>
-//   <div className={styles.row}>
-//     <li className={styles.label}>Artist</li>
-//     <li className={styles.info}>
-//       {record.artist.name}
-//     </li>
-//   </div>
-//   <div className={styles.row}>
-//     <li className={styles.label}>Condition</li>
-//     <li className={styles.info}>{record.condition}</li>
-//   </div>
-//   <div className={styles.row}>
-//     <li className={styles.label}>Year</li>
-//     <li className={styles.info}>{record.year}</li>
-//   </div>
-// </div>;
 
 export default EditModal;
